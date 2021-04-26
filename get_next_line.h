d95ed81fc7508aaaa84c961e0537b562b74f077f
Catia Filiberto <cfiliber@e4r4p1.42roma.it>
2021-04-26 18:22:59 +0200

    get_next_line_26_04

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 19:30:07 by cfiliber          #+#    #+#             */
-/*   Updated: 2021/03/17 19:30:14 by cfiliber         ###   ########.fr       */
+/*   Updated: 2021/04/26 18:19:08 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 4000
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
